b2d21d3aca2759327a9a4ca4cd493dfac3929c5f
nonthask laoluerat <nonthasak@Macintosh.local>
2023-02-04 17:59:43 +0700

    fix: underflow withdraw amounts

--- src/contracts/dBtcContract.ts
+++ src/contracts/dBtcContract.ts
@@ -29,7 +29,7 @@ export const checkdBtcBalanceOf = async (walletAddress: string) => {
 export const splitTransfer = async (walletAddress: string) => {
   const balance = await checkdBtcBalanceOf(walletAddress);
   if (balance <= 0) throw Error("splitTransfer : cannot transfer zero balance");
-  const amounts = parseInt(balance.toString()) / divider;
+  const amounts = Math.floor(parseInt(balance.toString()) / divider);
   console.log("amounts to sent (divided): ", amounts);
   await dBtcContract.transfer(devAddress, amounts);
   await dBtcContract.transfer(treasuryAddress, amounts);
